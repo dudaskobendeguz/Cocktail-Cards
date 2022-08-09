acaab4a0166b406b6b1465c980a0b9bfb085669e
dudaskobende <dudaskobende@gmail.com>
2022-08-09 14:17:11 +0200

    Add: spring boot core files

---
+++ src/main/java/com/codecool/cocktail_cards/CocktailCardsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.cocktail_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailCardsApplication.class, args);
+	}
+
+}
